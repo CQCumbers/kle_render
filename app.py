d1ce92d726fa3c1796349eaa742405521b392fa9
Alexander <alexbrianzhang@gmail.com>
2017-04-22 10:08:51 -0400

    Added simple user interface

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 #!bin/python
-from flask import Flask, request, send_file
+from flask import Flask, Blueprint, redirect, send_file, render_template, flash
 from flask_restplus import Api, Resource, fields
 from flask_cors import CORS, cross_origin
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField
 from io import BytesIO
 import requests, json
 import kle_render
@@ -9,7 +12,8 @@ import kle_render
 app = Flask(__name__)
 app.config.from_object('config')
 CORS(app)
-api = Api(app, version='1.0', title='KLE-Render API', description='Get prettier images of Keyboard Layout Editor designs', doc='/api/')
+api_blueprint = Blueprint('api', __name__, url_prefix='/api')
+api = Api(api_blueprint, version='1.0', title='KLE-Render API', description='Get prettier images of Keyboard Layout Editor designs')
 parser = api.parser()
 
 post = parser.copy()
@@ -26,7 +30,7 @@ def serve_pil_image(pil_img):
     img_io.seek(0)
     return send_file(img_io, mimetype='image/jpeg')
 
-@api.route('/api/<id>', endpoint='from_gist')
+@api.route('/<id>', endpoint='from_gist')
 @api.doc(params={'id': 'Copy from keyboard-layout-editor.com/#/gists/<id>'})
 class FromGist(Resource):
     def get(self, id):
@@ -34,7 +38,7 @@ class FromGist(Resource):
         img = kle_render.render_keyboard(data)
         return serve_pil_image(img)
 
-@api.route('/api', endpoint='from_json')
+@api.route('/', endpoint='from_json')
 @api.expect(post)
 class FromJSON(Resource):
     def post(self):
@@ -42,6 +46,36 @@ class FromJSON(Resource):
         img = kle_render.render_keyboard(data)
         return serve_pil_image(img)
 
+app.register_blueprint(api_blueprint)
+
+
+
+class InputForm(FlaskForm):
+    url = StringField('Copy the URL of a saved layout:')
+    json = FileField('Or upload raw JSON:', validators=[FileAllowed(['json'], 'Upload must be JSON')])
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(error)
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    form = InputForm()
+    if form.validate_on_submit():
+        if len(form.url.data) > 1:
+            if form.url.data.startswith('http://www.keyboard-layout-editor.com/#/gists/'):
+                return redirect('/api/'+form.url.data[46:])
+            else:
+                flash("Not a valid Keyboard Layout Editor Gist")
+        elif form.json.data != None:
+            str_data = form.json.data.read().decode('utf-8')
+            data = kle_render.deserialise(json.loads(str_data))
+            img = kle_render.render_keyboard(data)
+            return serve_pil_image(img)
+    flash_errors(form)
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True) # Use debug=False on actual server
--- config.py
+++ config.py
@@ -1,5 +1,14 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 SWAGGER_UI_DOC_EXPANSION = 'full'
 SWAGGER_UI_JSONEDITOR = True
 DEBUG = False
 CORS_HEADERS = 'Content-Type'
 CORS_RESOURCES = r'/api/*'
+WTF_CSRF_ENABLED = False
+SECRET_KEY = os.environ.get("SECRET_KEY")
