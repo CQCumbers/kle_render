9d4a31ffdc98aab291fde6e4e34e228f9bdfc926
Alexander <alexbrianzhang@gmail.com>
2017-05-15 22:27:17 -0400

    Improved GMK text rendering

--- key.py
+++ key.py
@@ -5,8 +5,6 @@ import math, textwrap
 
 # Key and deserialise() modified from https://github.com/jackhumbert/kle-image-creator
 class Key(object):
-    font_path = r'NotoRounded.otf'
-
     def __init__(self):
         self.x = 0.0
         self.y = 0.0
@@ -31,10 +29,16 @@ class Key(object):
         self.stepped = False
         self.decal = False
 
+    def font_path(self):
+        if self.profile in ['DCS', 'OEM', 'GMK']:
+            return 'CherryRounded.otf'
+        else:
+            return 'NotoRounded.otf'
+
     def base_img(self):
         if self.profile in ['DCS', 'OEM', 'GMK']: # GMK technically not supported by KLE yet
             return Image.open(r'GMK_base.jpg').convert('RGBA').resize((200, 200)) # GMK photo from official renders
-        elif self.profile in ['SA SPACE']:
+        elif 'SPACE' in self.profile:
             color = ImageColor.getrgb(self.color)
             bright = 0.3*color[0] + 0.59*color[1] + 0.11*color[2]
             if (bright > 0x50):
@@ -60,7 +64,7 @@ class Key(object):
     def base_color(self):
         if self.profile in ['DCS', 'OEM', 'GMK']: # GMK technically not supported by KLE yet
             return 0xE0
-        elif self.profile in ['SA SPACE']:
+        elif 'SPACE' in self.profile:
             color = ImageColor.getrgb(self.color)
             bright = 0.3*color[0] + 0.59*color[1] + 0.11*color[2]
             if (bright > 0x50):
@@ -151,7 +155,7 @@ class Key(object):
         line_spacing = 16
         labels = [text.upper() for text in self.labels] # only uppercase text on SA
 
-        gotham = ImageFont.truetype(self.font_path, int(self.font_size*scale_factor)+min_size)
+        gotham = ImageFont.truetype(self.font_path(), int(self.font_size*scale_factor)+min_size)
         w = max([gotham.getsize(text)[0] for text in labels]) # max of widths
         h = sum([gotham.getsize(text)[1] for text in labels]) # sum of heights
         h += line_spacing*(len([text for text in labels if len(text) > 0])-1)
@@ -161,7 +165,7 @@ class Key(object):
 
     def text_key(self, key_img): # convert for use with list of labels
         labels = self.labels
-        labels = [labels[i].upper() for i in range(len(labels)) if len(labels[i]) > 0 and i not in (4,5)] # only uppercase text on SA - ignore blank lines and labels on front (not top) of key
+        labels = [labels[i] for i in range(len(labels)) if len(labels[i]) > 0 and i not in (4,5)] # only uppercase text on SA - ignore blank lines and labels on front (not top) of key
         if len(labels) < 1:
             return key_img # if blank, exit immediately
 
@@ -174,9 +178,17 @@ class Key(object):
         else:
             offset = 12 # pixels to shift text upwards to center it in keycap top
             alignment = 'center'
-        width_limit = key_img.width - 42 # 60 is combined width of keycap sides in base image
+        width_limit = key_img.width - 42 # 42 is combined width of keycap sides in base image
 
-        gotham = ImageFont.truetype(self.font_path, int(self.font_size*scale_factor)+min_size)
+        if self.profile in ['DCS', 'OEM', 'GMK']:
+            scale_factor = 4
+            min_size = 8
+            width_limit = key_img.width - 55
+            alignment = 'left'
+        else:
+            labels = [labels[i].upper() for i in range(len(labels))]
+
+        gotham = ImageFont.truetype(self.font_path(), int(self.font_size*scale_factor)+min_size)
         draw = ImageDraw.Draw(key_img)
         w = max([gotham.getsize(text)[0] for text in labels]) # max of label widths
         if w > width_limit and not self.decal:
@@ -186,7 +198,11 @@ class Key(object):
         h += line_spacing*(len([text for text in labels])-1)
         c = ImageColor.getrgb(self.font_color)
         c = tuple(band + 0x26 for band in c) # Simulates reflectivity
-        draw.multiline_text((int((key_img.width-w)/2), int((key_img.height-h)/2 - offset)), '\n'.join(labels), font=gotham, fill=c, spacing=line_spacing, align=alignment)
+
+        if self.profile in ['DCS', 'OEM', 'GMK']:
+            draw.multiline_text((int(45), int(30)), '\n'.join(labels), font=gotham, fill=c, spacing=line_spacing, align=alignment)
+        else:
+            draw.multiline_text((int((key_img.width-w)/2), int((key_img.height-h)/2 - offset)), '\n'.join(labels), font=gotham, fill=c, spacing=line_spacing, align=alignment)
         return key_img 
 
     def render(self):
